4b30b028fb9ca717298178f50241ea052491af55
symptots meanstack <symptotsmeanstack@gmail.com>
2021-01-27 13:59:11 +0530

    commitMessage

--- pushing.sh
+++ pushing.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 git add .
 git commit -m "commitMessage"
+expect "Enter username\r"
+ 
+send -- "abijithksym\r"
+ 
+expect "Enter password\r"
+ 
+send -- "symptots123\r"
 git push 
 
  
