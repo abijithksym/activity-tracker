46332a5e41e50a6bf8f5768236d47e69c0c77439
symptots meanstack <symptotsmeanstack@gmail.com>
2021-01-27 17:08:41 +0530

    message1

--- pushed.sh
+++ pushed.sh
@@ -7,5 +7,7 @@ git commit -m "message1"
 # else
 git status
 echo "Pushing data to remote server!!!"
-git push -u origin set-url "https://abijithksym@github.com"
+username = "abijithksym"
+password = "symptots123"
+git push -u origin set-url " https://{username}:{password}@github.com/abijithksym/activity-tracker.git"
 # fi
