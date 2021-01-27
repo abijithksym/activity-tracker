c661211c3c382e48f65ca2e59b840557b4f1143d
symptots meanstack <symptotsmeanstack@gmail.com>
2021-01-27 16:24:28 +0530

    message1

--- pushed.sh
+++ pushed.sh
@@ -1,11 +1,11 @@
 
 git add .
 git commit -m "message1"
-if [ -n "$(git status - porcelain)" ];
-then
- echo "IT IS CLEAN"
-else
- git status
- echo "Pushing data to remote server!!!"
- git push -u origin
-fi
+# if [ -n "$(git status - porcelain)" ];
+# then
+#  echo "IT IS CLEAN"
+# else
+git status
+echo "Pushing data to remote server!!!"
+git push -u origin
+# fi
