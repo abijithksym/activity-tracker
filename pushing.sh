98d13db928b11d6e7f7847e6fe219b74d9913be8
symptots meanstack <symptotsmeanstack@gmail.com>
2021-01-27 15:42:58 +0530

    message

--- pushing.sh
+++ pushing.sh
@@ -2,11 +2,23 @@
 
  
 
-git add .
-git commit -m "commitMessage"
+# git add .
+# git commit -m "commitMessage"
 
  
-send -- "abijithksym\r"
+# send -- "abijithksym\r"
 
-send -- "symptots123\r"
-git push 
+# send -- "symptots123\r"
+# git push 
+#!/usr/bin/expect -f
+spawn ssh aspen
+git add .
+git commit -m "message"
+git push
+expect "user name"
+send "abijithksym\r"
+
+expect "password: "
+send "symptots123\r"
+
+send "exit\r"
