aad9488e70a2aba723561327acea1e0a71c59aec
symptots meanstack <symptotsmeanstack@gmail.com>
2021-01-28 11:01:32 +0530

    Update -  $(date)

--- bash1.sh
+++ bash1.sh
@@ -6,15 +6,14 @@ function block_for_change {
     $DIRECTORY_TO_OBSERVE
 }
 
+
+while block_for_change; do
 git add --all
 git rm $(git ls-files --deleted)
 git commit -m 'Update -  $(date)'
 git push -u origin master
-# Keeping the 'expect' code with '-c' flag
-expect -c"
-exp_internal 1; #Remove this line once your prob solved
-expect \"Username:\"
-send \"TestUsernme\r\n\"
-expect \"Password:\"
-send \"TestPassword\r\n\"
-"
+expect "Username:"
+send "TestUsernme\r\n"
+expect "Password:"
+send "TestPassword\r\n"
+done
